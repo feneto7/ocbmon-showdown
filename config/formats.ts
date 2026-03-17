2ce3126a79fc24c6e912e9ea805445baa516f93e
feneto7 <franciscofelipesn@gmail.com>
2026-03-16 22:24:17 -0300

    obtainable

--- config/formats.ts
+++ config/formats.ts
@@ -32,7 +32,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		name: "[Gen 9] OCBMons",
 		mod: 'gen9ocb',
 		ruleset: [
-			'Obtainable', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause',
+			'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause',
 			'Sleep Clause Mod', 'OHKO Clause',
 			'+Custom', '+Nonexistent', 'Dynamax Clause',
 		],
